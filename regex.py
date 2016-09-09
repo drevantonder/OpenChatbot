ef2c0df0c48ed5cd4ecb60c11ff719b4e2bcac57
Andre van Tonder <drevtonder@gmail.com>
2016-09-09 14:52:09 +0800

    Create regex.py

---
+++ regex.py
@@ -0,0 +1,5 @@
+io = [
+    # Create answer and quetsions here
+    #('what is your name', 'My name is {my_name} Augustus the fourth, BUT please call me {my_name}'),
+    #('what is the date', '{datetime}')
+]
